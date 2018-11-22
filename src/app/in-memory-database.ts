48bca007b7ec02825296b593c554bafb054ec8ae
anna vitoria <vitoria@dimensiva.online>
2018-11-22 19:11:40 -0300

    Adicionado modelo de category

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,9 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDataBase implements InMemoryDbService{
     createDb(){
-        const categories = [
+        const categories:Category[] = [
             {id:1, nome:"Lazer", descricao:"Cinema, Parque, Praia"},
             {id:2, nome:"Saúde", descricao:"Plano de Saúde e Remédios"},
             {id:3, nome:"Moradia", descricao:"Pagamentos de Contas de casa"},
